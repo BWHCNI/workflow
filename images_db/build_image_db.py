722c3e5c73d1c1bdd48fb4798435eda2848c476b
Collin Poczatek <collin.poczatek@gmail.com>
2013-03-28 21:34:36 +0000

    commit of actual script. still needs lots of work
    git-svn-id: https://zoomwhee.mgh.harvard.edu/svn_devel/workflow_scripts@45 e697587a-9014-4c0e-a5eb-71ab1d676c3b

---
+++ images_db/build_image_db.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+import fnmatch
+import os
+import hashlib
+import time
+import sqlite3 as lite
+import sys
+
+#
+# READ THIS!
+# This script assumes several things:
+# - db file is in this directory
+# - db allready has a table Images generated with:
+# CREATE TABLE Images( id INTEGER PRIMARY KEY, fname TEXT, path TEXT, size INTEGER, mtime TEXT, own INTEGER, perm TEXT, hash TEXT );
+# - need to set the search root 'start'
+# - need to set image extensions
+# - as of now there is no check to see if file is in db
+#
+# has been run against:
+#
+
+start = "/nrims/home3/cpoczatek/"
+extensions = ('.im','.nrrd')
+con = lite.connect('image_data.db')
+cur = con.cursor()
+
+for root, dirnames, filenames in os.walk(start):
+  for filename in filenames:
+    if filename.endswith(extensions):
+      # get full path
+      fullpath = os.path.join(root, filename)
+      
+      # computing the hash piece by piece is more memory efficient
+      #This is too slow, commenting out
+#      md5 = hashlib.md5()
+#      f = open(fullpath, 'r')
+#      while True:
+#        data = f.read(2**14)
+#        if not data:
+#            break
+#        md5.update(data)
+#      psum = md5.hexdigest()
+
+      # get file metadata
+      info = os.stat(fullpath)
+      # format modification time
+      modtime = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(info.st_mtime))
+      # get owner/permissions
+      owner = info.st_uid
+      perm = oct(info.st_mode)[-3:]
+
+      stats = (filename, fullpath, info.st_size, modtime, owner, perm)
+      print stats
+      
+      cur.execute("INSERT INTO Images(fname, path, size, mtime, own, perm) VALUES(?,?,?,?,?,?)", stats )
+      con.commit()
+
+
+
