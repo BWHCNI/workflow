ac54772b75096924594946f4cee672bb434a2277
Collin Poczatek <collin.poczatek@gmail.com>
2013-03-28 21:34:05 +0000

    commit of notes and changes to test script
    git-svn-id: https://zoomwhee.mgh.harvard.edu/svn_devel/workflow_scripts@44 e697587a-9014-4c0e-a5eb-71ab1d676c3b

--- images_db/find_images_test.py
+++ images_db/find_images_test.py
@@ -10,37 +10,46 @@ paths = []
 md5sums = []
 
 start = "/nrims/home3/cpoczatek/Pictures/"
+extensions = ('.jpg', '.jpeg', '.gif', '.png')
 
 for root, dirnames, filenames in os.walk(start):
-  for filename in fnmatch.filter(filenames, '*.jpg'):
+  for filename in filenames:
+    if filename.endswith(extensions):
+      
+      # found a matching file
+      matches.append(filename)
+      
+      # get full path
+      fullpath = os.path.join(root, filename)
+      paths.append(fullpath)
 
-    # found a matching file
-    matches.append(filename)
-    
-    # get full path
-    fullpath = os.path.join(root, filename)
-    paths.append(fullpath)
+      # both this and the method below compute the same hash
+      #
+      #file_md5sum = hashlib.md5(open(fullpath, 'r').read()).hexdigest()
+      #md5sums.append(file_md5sum)
 
-    # both this and the method below compute the same hash
-    #
-    #file_md5sum = hashlib.md5(open(fullpath, 'r').read()).hexdigest()
-    #md5sums.append(file_md5sum)
+      # computing the hash bit by bit is more memory efficient
+      md5 = hashlib.md5()
+      f = open(fullpath, 'r')
+      while True:
+        data = f.read(2**14)
+        if not data:
+            break
+        md5.update(data)
+      psum = md5.hexdigest()
 
-    # computing the hash bit by bit is more memory efficient
-    md5 = hashlib.md5()
-    f = open(fullpath, 'r')
-    while True:
-      data = f.read(2**14)
-      if not data:
-          break
-      md5.update(data)
-    psum = md5.hexdigest()
-
-    # get file metadata
-    info = os.stat(fullpath)
-    # format modification time
-    modtime = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(info.st_mtime))
-
-    print (filename, fullpath, info.st_size, modtime, psum)
+      # get file metadata
+      info = os.stat(fullpath)
+      # format modification time
+      modtime = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(info.st_mtime))
+      
+      # get ownership and permissions
+      own = info.st_uid
+      perm = oct(info.st_mode)[-3:]
+      stats = (filename, fullpath, info.st_size, modtime, own, perm, psum)
+      print stats
 
 print "\n\nfin.\n"
+
+
+
